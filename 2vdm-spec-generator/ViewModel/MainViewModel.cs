ce7304eb9c693e308237293528c4c689a2718c9d
bizyutyu <bizyuness.takakura@gmail.com>
2024-11-01 17:35:29 +0900

    add: チュートリアルの変更を追加

---
+++ 2vdm-spec-generator/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace _2vdm_spec_generator.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string text;
+
+        public string Text
+        {
+            get => text;
+            set
+            {
+                text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
