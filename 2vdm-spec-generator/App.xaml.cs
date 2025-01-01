59392c821a17d37c6c800296a5ed2423f826e81a
bizyutyu <bizyuness.takakura@gmail.com>
2025-01-01 21:47:42 +0900

    アプリ起動時のエラーハンドリングとエラー時出力の追加等

--- 2vdm-spec-generator/App.xaml.cs
+++ 2vdm-spec-generator/App.xaml.cs
@@ -2,11 +2,29 @@
 {
     public partial class App : Application
     {
-        public App(IServiceProvider services)
+        public App(IServiceProvider service)
         {
-            InitializeComponent();
-
-            MainPage = new AppShell(services);
+            try
+            {
+                InitializeComponent();
+                MainPage = new AppShell(service);
+            }
+            catch (Exception ex)
+            {
+                // エラーメッセージをポップアップで表示
+                MainPage = new ContentPage
+                {
+                    Content = new VerticalStackLayout
+                    {
+                        Children =
+                        {
+                            new Label { Text = "アプリケーションの初期化中にエラーが発生しました:" },
+                            new Label { Text = ex.Message },
+                            new Label { Text = ex.StackTrace }
+                        }
+                    }
+                };
+            }
         }
     }
 }
--- 2vdm-spec-generator/MainPage.xaml.cs
+++ 2vdm-spec-generator/MainPage.xaml.cs
@@ -5,10 +5,31 @@ namespace _2vdm_spec_generator
     public partial class MainPage : ContentPage
     {
 
-        public MainPage(MainViewModel vm)
+        public MainPage(MainViewModel viewModel)
         {
-            InitializeComponent();
-            BindingContext = vm;
+            try
+            {
+                InitializeComponent();
+                BindingContext = viewModel;
+            }
+            catch (Exception ex)
+            {   
+                // エラーメッセージをコンソールに出力
+                // なぜだか以下のコンソールログ出力機能を追加するとエラーが発生しなくなった（気のせいかもしれないが一応メモ）
+                Console.WriteLine($"ページの初期化中にエラーが発生しました:");
+                Console.WriteLine($"エラーメッセージ: {ex.Message}");
+                Console.WriteLine($"スタックトレース: {ex.StackTrace}");
+                // エラーメッセージを表示
+                Content = new VerticalStackLayout
+                {
+                    Children =
+                    {
+                        new Label { Text = "ページの初期化中にエラーが発生しました:" },
+                        new Label { Text = ex.Message },
+                        new Label { Text = ex.StackTrace }
+                    }
+                };
+            }
         }
 
     }
