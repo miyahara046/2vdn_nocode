1a2d692b22432ccc701dbf46eb16cddf8e227595
miyahara046 <taka.rocket.nba.1104@gmail.com>
2025-09-28 21:17:11 +0900

    update:ファイル一覧を階層構造に

---
+++ 2vdm-spec-generator/Models/FolderItem.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace _2vdm_spec_generator.ViewModel
+{
+    public class FolderItem : ObservableObject
+    {
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+        public int Level { get; set; } = 0;
+
+        private bool _isExpanded = true;
+        public bool IsExpanded
+        {
+            get => _isExpanded;
+            set
+            {
+                if (SetProperty(ref _isExpanded, value))
+                    UpdateChildrenVisibility();
+            }
+        }
+
+        private bool _isVisible = true;
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set => SetProperty(ref _isVisible, value);
+        }
+
+        public bool IsFolder => Directory.Exists(FullPath);
+        public bool IsFile => File.Exists(FullPath);
+
+        public ObservableCollection<FolderItem> Children { get; } = new();
+
+        private void UpdateChildrenVisibility()
+        {
+            foreach (var child in Children)
+            {
+                child.IsVisible = this.IsExpanded;
+                if (child.IsFolder)
+                    child.UpdateChildrenVisibility();
+            }
+        }
+    }
+}
--- 2vdm-spec-generator/NoCodePage.xaml.cs
+++ 2vdm-spec-generator/NoCodePage.xaml.cs
@@ -1,5 +1,4 @@
 using Microsoft.Maui.Controls;
-using System.IO;
 
 namespace _2vdm_spec_generator
 {
@@ -9,23 +8,6 @@ namespace _2vdm_spec_generator
         {
             InitializeComponent();
             BindingContext = new ViewModel.NoCodePageViewModel();
-        }
-
-        private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (BindingContext is ViewModel.NoCodePageViewModel vm &&
-                e.CurrentSelection.FirstOrDefault() is string selectedFile)
-            {
-                vm.SelectedFileName = selectedFile;
-
-                // Markdown内容を読み込む
-                string path = Path.Combine(vm.SelectedFolderPath, selectedFile);
-                vm.MarkdownContent = File.Exists(path) ? File.ReadAllText(path) : string.Empty;
-
-                // VDM++内容も更新
-                string vdmPath = Path.ChangeExtension(path, ".vdmpp");
-                vm.VdmContent = File.Exists(vdmPath) ? File.ReadAllText(vdmPath) : string.Empty;
-            }
         }
     }
 }
